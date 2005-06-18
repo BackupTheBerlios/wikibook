95c4afa45ff9324cbdd4b7126fe142adba04898b
bastie <bastie>
2005-06-18 19:38:55 +0000

    Initial revision

---
+++ wikibook/de/bastie/wiki/book/sample/screenshot/Fenster.java
@@ -0,0 +1,17 @@
+package de.bastie.wiki.book.sample.screenshot;
+
+import javax.swing.JFrame;
+
+/**
+ * Klasse für den Wikibook Screenshot
+ * @author Bastie - Sebastian Ritter
+ */
+public class Fenster {
+
+  public static void main (final String [] ignored) {
+    JFrame fenster = new JFrame ("WikiBooks");
+    fenster.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+    fenster.setSize(150,100);
+    fenster.setVisible(true);
+  }
+}
